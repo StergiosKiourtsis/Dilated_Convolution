2782e5355d6b96dc307bca37edf06c8d224ea3a3
StergiosKiourtsis <94305931+StergiosKiourtsis@users.noreply.github.com>
2022-01-23 22:55:20 +0200

    Update dilated_conv.cc

--- dilated_conv.cc
+++ dilated_conv.cc
@@ -1,48 +1,59 @@
-#include <iostream>
-
-int *dilate(int *kernel,int rows,int columns,int degree){
-    int N=(rows-1)*(degree-1)+rows;
-    int M=(columns-1)*(degree-1)+columns;
-    int *filter = new int[N*M];
-    filter[-1]=N;
-    filter[-2]=M;
+template<int N,int M,int R,int C>
+void dilate(int in_filter[N][M],int out_filter[R][C],int degree){
     int count=0;
-    for(int i=0;i<N*M;i++){
-        if((i/M)%degree==0 && (i%M)%degree==0){
-            filter[i]=kernel[count];
-            count++;
-        }else{
-            filter[i]=0;
-        }
-    }
-    return filter;
+    for(int i=0;i<R;i++){
+		for(int j=0;j<C;j++){
+			if(i%degree==0 && j%degree==0){
+				out_filter[i][j]=in_filter[count/M][count%M];
+				count++;
+			}else{
+				out_filter[i][j]=0;
+			}
+		}
+	}
 }
 
-int *conv(int *image,int *filter, int rows, int columns){ 
-    int filter_size=filter[-1];
-    int pixel; 
-    int kx=filter_size/2;
-    int ky=filter_size/2;
-    int i,j,ii,jj,k,l;
-    int dim = rows*columns;
-    int *new_image = new int[dim];
-    for(int pixel_number=0;pixel_number<dim;pixel_number++){
-        i = pixel_number/columns;
-        j = pixel_number%columns;
-        pixel =0;
-        for(int filter_pixel=0;filter_pixel<filter_size*filter_size;filter_pixel++){
-            k = filter_pixel/filter_size;
-            l = filter_pixel%filter_size;
-            ii=i+kx-k;
-            jj=j+kx-l;
-            if(ii>=0 && ii<rows && jj>=0 && jj<columns){
-                pixel+=image[ii*columns+jj]*filter[k*filter_size+l];
-            }else{
-                pixel +=0;
-            }
-        }
-        new_image[i*columns+j]=pixel;
-    }
-	
-    return new_image;
-}
+template<int N,int M,int R,int C>
+void convBuf(int image[N][M],int filter[R][C],int result[N][M]){ 
+	int line_buffer[R][M];
+    int pixel;
+    int kx=R/2;
+    int i,j,ii,jj,k,l,r; 
+    int dim = N*M; 	
+	int count=kx+1;
+    for(i=0;i<N;i++){
+		if(i==0){
+			for(r=0; r<count; r++){
+				for(j=0; j<M; j++){
+					line_buffer[r][j] = image[i+r][j];
+				}
+			}
+		}
+		else if(i<N-R){
+			for(j=0; j<M; j++){
+				line_buffer[0][j] = image[count][j];
+			}
+			for(r=1; r<R; r++){
+				for(j=0; j<M; j++){
+						line_buffer[r][j] = line_buffer[r-1][j];
+				}
+			}
+		}
+		for(j=0;j<M;j++){
+			pixel=0;
+			for(k=0;k<R;k++){
+				for(l=0;l<C;l++){
+					ii=i+kx-k;
+					jj=j+kx-l;
+					if(ii>=0 && ii<N && jj>=0 && jj<M){
+						pixel+=line_buffer[k][jj]*filter[k][l]; 
+					}else{ 
+					pixel +=0; 
+					} 
+				} 
+			}
+		result[i][j]=pixel;
+		}
+		count++;
+	}
+ }
