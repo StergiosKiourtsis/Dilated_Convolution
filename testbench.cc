f31ffa011251303a09969b59f3839b3f8fd61e9f
StergiosKiourtsis <94305931+StergiosKiourtsis@users.noreply.github.com>
2022-01-23 22:57:48 +0200

    Update testbench.cc

--- testbench.cc
+++ testbench.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "dv.cc"
+#include "dvNew.cc"
 
 int main(){
 
@@ -16,65 +16,27 @@ int main(){
                        {1,2,3,4,5,6,7,8,9,10},
                        {1,2,3,4,5,6,7,8,9,10},
                        {1,2,3,4,5,6,7,8,9,10}};
-    int *result;
-    int *p;
-////////////////////////////////////////////////////////////////////////////////////////////
-//TEST 2 WITH A 3*3 FILTER AND 2 DEGREES OF DILATION 
-////////////////////////////////////////////////////////////////////////////////////////////// 
-    std::cout<<"\n3x3 Filter dilate 2 degrees:";                                                    
-    p = dilate(filter[0],3,3,2);         
-    result=conv(array[0],p,10,10);                                  
-    for(int i=0;i<p[-1]*p[-2];i++){               //                
-            if(i%p[-1]==0){ std::cout<<"\n";}     //     print out the new filter                 
-            else{std::cout<<" ";}                 //                             
-            std::cout<<p[i];                      //                
-    }
-  std::cout<<"\n3x3 Filter Dilation Degree:2 Result:";
-   for(int i=0;i<sizeof(array)/sizeof(int);i++){ //                      
-       if(i%10==0){ std::cout<<"\n";}            //   print out the results of convlution                              
-       else{std::cout<<" ";}                     //                        
-       std::cout<<result[i];                     //                        
-   }                                     
-/////////////////////////////////////////////////////////////////////////////////////////////
-//TEST 2 WITH A 3*3 FILTER AND 3 DEGREES OF DILATION 
-////////////////////////////////////////////////////////////////////////////////////////////// 
- std::cout<<"\n3x3 Filter dilate 3 degrees:";                              
-  p = dilate(filter[0],3,3,3);                              
- result=conv(array[0],p,10,10);                              
-  for(int i=0;i<p[-1]*p[-2];i++){               //    
-          if(i%p[-1]==0){ std::cout<<"\n";}     //     print out the new filter                  
-          else{std::cout<<" ";}                 //             
-          std::cout<<p[i];                      //        
-  }
-  std::cout<<"\n3x3 Filter Dilation Degree:3 Result:";                                                                                                         
-   for(int i=0;i<sizeof(array)/sizeof(int);i++){         // 
-       if(i%10==0){ std::cout<<"\n";}                    // print out the results of convlution                          
-       else{std::cout<<" ";}                             //       
-       std::cout<<result[i];                             //       
-   }   
-////////////////////////////////////////////////////////////////////////////////////////////// 
-//TEST 3 WITH A 5*5 FILTER AND 2 DEGREES OF DILATION 
-////////////////////////////////////////////////////////////////////////////////////////////// 
-  int filter1[5][5]={{1,1,1 ,1,1},
-                    {1,1,1 ,1,1},
-                    {1,1,-2,1,1},
-                    {1,1,1 ,1,1},
-                    {1,1,1 ,1,1}};
- std::cout<<"\n5x5 Filter dilate 2 degrees:";                        
-  p = dilate(filter1[0],5,5,2);                        
-  result=conv(array[0],p,10,10);                        
-  for(int i=0;i<p[-1]*p[-2];i++){             //  
-          if(i%p[-1]==0){ std::cout<<"\n";}   //    print out the new filter             
-          else{std::cout<<" ";}               //         
-          std::cout<<p[i];                    //    
-  }
-   std::cout<<"\n5x5 Filter Dilation Degree:2 Result:";                                                            
-    for(int i=0;i<sizeof(array)/sizeof(int);i++){        // 
-        if(i%10==0){ std::cout<<"\n";}                   //  print out the results of convlution          
-        else{std::cout<<" ";}                            //   
-        std::cout<<result[i];                            //   
-    }   
+    int result1[10][10]; 
+    int result2[10][10];
+    //new_rows = (rows-1)*(degree-1)+rows
+    //(3-1)*(2-1)+3 = 5
+    int dil_filter[5][5];
 
-    return 0;
-}
-
+//TEST 2 WITH A 3*3 FILTER AND 2 DEGREES OF DILATION  
+    std::cout<<"\n3x3 Filter dilate 2 degrees: \n";                                                    
+    dilate<3,3,5,5>(filter,dil_filter,2);         
+    for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+				std::cout<<dil_filter[i][j]<<" ";
+		}
+		std::cout<<"\n";
+	}
+    
+    std::cout<<"\n BUFFER TIME! \n";
+    convBuf<10,10,5,5>(array,dil_filter,result2);
+   for(int i=0;i<10;i++){
+		for(int j=0;j<10;j++){
+				std::cout<<result2[i][j]<<" ";
+		}
+		std::cout<<"\n";
+	}
