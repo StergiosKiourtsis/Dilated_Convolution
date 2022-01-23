cad11427440e645a0b3e9680b338be7668e7c728
StergiosKiourtsis <94305931+StergiosKiourtsis@users.noreply.github.com>
2022-01-23 23:03:42 +0200

    Update testbench.cc

--- testbench.cc
+++ testbench.cc
@@ -16,8 +16,7 @@ int main(){
                        {1,2,3,4,5,6,7,8,9,10},
                        {1,2,3,4,5,6,7,8,9,10},
                        {1,2,3,4,5,6,7,8,9,10}};
-    int result1[10][10]; 
-    int result2[10][10];
+    int result[10][10];
     //new_rows = (rows-1)*(degree-1)+rows
     //(3-1)*(2-1)+3 = 5
     int dil_filter[5][5];
@@ -33,10 +32,10 @@ int main(){
 	}
     
     std::cout<<"\n BUFFER TIME! \n";
-    convBuf<10,10,5,5>(array,dil_filter,result2);
+    convBuf<10,10,5,5>(array,dil_filter,result);
    for(int i=0;i<10;i++){
 		for(int j=0;j<10;j++){
-				std::cout<<result2[i][j]<<" ";
+				std::cout<<result[i][j]<<" ";
 		}
 		std::cout<<"\n";
 	}
