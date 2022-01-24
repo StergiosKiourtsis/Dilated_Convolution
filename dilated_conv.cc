c284ee11ed1fcb994687cb8422eda6e4c2340a0a
StergiosKiourtsis <94305931+StergiosKiourtsis@users.noreply.github.com>
2022-01-24 10:20:39 +0200

    Update dilated_conv.cc

--- dilated_conv.cc
+++ dilated_conv.cc
@@ -62,31 +62,3 @@ void convBuf(int image[N][M],int filter[R][C],int result[N][M]){
 		}
 	}
  }
-
-
- template<int N,int M,int R,int C>
-void conv(int image[N][M],int filter[R][C],int result[N][M]){ 
-	int line_buffer[R][M];
-    int pixel;
-    int kx=R/2;
-    int i,j,ii,jj,k,l; 
-    int dim = N*M; 
-
-    for(i=0;i<N;i++){
-		for(j=0;j<M;j++){
-			pixel=0;
-			for(k=0;k<R;k++){
-				for(l=0;l<C;l++){
-					ii=i+kx-k;
-					jj=j+kx-l;
-					if(ii>=0 && ii<N && jj>=0 && jj<M){
-						pixel+=image[ii][jj]*filter[k][l]; 
-					}else{ 
-					pixel +=0; 
-					} 
-				} 
-			}
-		result[i][j]=pixel;
-		}
-	}
- }
