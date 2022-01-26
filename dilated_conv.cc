652aab68da68d2e5301b5e630842bc59de17e646
StergiosKiourtsis <sterkiourtsis@gmail.com>
2022-01-26 15:26:40 +0200

    m

--- dilated_conv.cc
+++ dilated_conv.cc
@@ -1,38 +1,54 @@
+#include "ac_channel.h"
+
 template<typename T,int N,int M,int R,int C>
-void dilate(T in_filter[N][M],T out_filter[R][C],T degree){
+void dilate(T in_filter[N][M],T out_filter[R][C],T degree,ac_channel<T> &line_in,ac_channel<T> &line_out){
     int count=0;
     for(int i=0;i<R;i++){
 		for(int j=0;j<C;j++){
 			if(i%degree==0 && j%degree==0){
-				out_filter[i][j]=in_filter[count/M][count%M];
+				
+				line_in.write(in_filter[count/M][count%M]);
+				line_out.read(out_filter[i][j]);
+				//out_filter[i][j]=in_filter[count/M][count%M];
 				count++;
 			}else{
-				out_filter[i][j]=0;
+				line_in.write(0);
+				line_out.read(out_filter[i][j]);
+				//out_filter[i][j]=0;
 			}
 		}
 	}
 }
 
 template<typename T,int N,int M,int R,int C>
-void convBuf(T image[N][M],T filter[R][C],T result[N][M]){ 
+void convBuf(T image[N][M],T filter[R][C],T result[N][M],ac_channel<T> &line_in,ac_channel<T> &line_out,ac_channel<T> &line_buff_in,ac_channel<T> &line_buff_out){ 
 	T line_buffer[R][M];
     T pixel;
     T kx=R/2;
     T i,j,ii,jj,k,l,r; 
-    T dim = N*M; 	
-	T count=kx+1;
+	T tmp;
     for(i=0;i<N;i++){
 		if(i==0){
 			for(r=0; r<=kx; r++){
 				for(j=0; j<M; j++){
-					line_buffer[R-1-r][j] = image[kx-r][j];
+					line_in.write(image[kx-r][j]);
+					line_out.read(tmp);
+					line_buff_in.write(tmp);
+					line_buff_out.read(line_buffer[R-1-r][j]);
+					//line_buffer[R-1-r][j] = image[kx-r][j];
 				}
 			}
 		}else if(i<N-kx && i>0){
 			for(r=0; r<R; r++){
 				for(j=0; j<M; j++){
 						if(r==R-1){
-							line_buffer[r][j] = image[i+kx][j];
+							
+							line_in.write(image[i+kx][j]);
+							line_out.read(tmp);
+							line_buff_in.write(tmp);
+							line_buff_out.read(line_buffer[r][j]);							
+							//line_buffer[r][j] = image[i+kx][j];
+							
 						}else{
 							line_buffer[r][j] = line_buffer[r+1][j];
 						}
@@ -52,13 +68,16 @@ void convBuf(T image[N][M],T filter[R][C],T result[N][M]){
 					ii=i-kx+k;
 					jj=j-kx+l;
 					if(ii>=0 && ii<N && jj>=0 && jj<M){
-						pixel+=line_buffer[k][jj]*filter[k][l]; 
+						line_buff_in.write(line_buffer[k][jj]);
+						line_in.write(filter[k][l]);
+						pixel+=line_buff_out.read()*line_out.read(); 
 					}else{ 
 					pixel +=0; 
 					} 
 				} 
 			}
-		result[i][j]=pixel;
+		line_in.write(pixel);
+		line_out.read(result[i][j]);
 		}
 	}
  }
--- testbench.cc
+++ testbench.cc
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "dilated_conv.cc"
-#include"ac_int.h"
+#include "ac_int.h"
 
 
 int main(){
 
   ac_int<8,false> filter[3][3]={{1,1,1 },
-                                {1,-2,1},
+                                {1,3,1},
                                 {1,1,1 }};
+                                
   ac_int<8,false> array[10][10]={{1,1,1,1,1,14,1,51,1,1},
                                  {2,15,2,1,2,14,2,2,2,8},
                                  {3,2,3,1,23,14,3,3,6,3},
@@ -18,7 +19,11 @@ int main(){
                                  {3,3,3,1,3,14,3,3,33,3},
                                  {4,4,7,1,4,14,4,20,9,4},
                                  {21,5,5,1,5,14,5,5,5,5}};
-
+  
+  ac_channel<ac_int<8,false> > line_in;
+  ac_channel<ac_int<8,false> > line_buff_in;
+  ac_channel<ac_int<8,false> > line_out;
+  ac_channel<ac_int<8,false> > line_buff_out;
   ac_int<8,false> result1[10][10]; 
   ac_int<8,false> result2[10][10];
 
@@ -28,7 +33,7 @@ int main(){
 
   //TEST 2 WITH A 3*3 FILTER AND 3 DEGREES OF DILATION  
   std::cout<<"\n3x3 Filter dilate 2 degrees: \n";                                                    
-  dilate<ac_int<8,false>,3,3,7,7>(filter,dil_filter,3);         
+  dilate<ac_int<8,false>,3,3,7,7>(filter,dil_filter,3,line_in,line_out);         
   for(int i=0;i<7;i++){
 	  for(int j=0;j<7;j++){
 	  		std::cout<<dil_filter[i][j]<<" ";
@@ -37,7 +42,7 @@ int main(){
   }
 
   std::cout<<"\n BUFFER TIME! \n";
-  convBuf<ac_int<8,false>,10,10,7,7>(array,dil_filter,result2);
+  convBuf<ac_int<8,false>,10,10,7,7>(array,dil_filter,result2,line_in,line_out,line_buff_in,line_buff_out);
   for(int i=0;i<10;i++){
 	  for(int j=0;j<10;j++){
 	  	std::cout<<result2[i][j]<<" ";
