c5a8a2492a2a1757cc28df255b8037efc6f707e2
StergiosKiourtsis <94305931+StergiosKiourtsis@users.noreply.github.com>
2022-02-09 01:58:23 +0200

    Update testbench.cc

--- testbench.cc
+++ testbench.cc
@@ -21,30 +21,14 @@ int main(){
                                 {21,5,5,1,5,14,5,5,5,5}};
   
 
-  ac_int<8,false> result1[10][10]; 
-  ac_int<8,false> result2[10][10];
+  ac_int<8,false> result[10][10];
 
   //new_rows = (rows-1)*(degree-1)+rows
   //int size = (3-1)*(2-1)+3;
   ac_int<8,false> dil_filter[7][7];
 
-  //TEST 2 WITH A 3*3 FILTER AND 3 DEGREES OF DILATION  
-  std::cout<<"\n3x3 Filter dilate 2 degrees: \n";                                                    
-  dilate<ac_int<8,false>,3,3,7,7>(filter,dil_filter,3);         
-  for(int i=0;i<7;i++){
-	  for(int j=0;j<7;j++){
-	  		std::cout<<dil_filter[i][j]<<" ";
-	  }
-	  std::cout<<"\n";
-  }
+  dilate<ac_int<8,false>,3,3,7,7>(filter,dil_filter,3);  	
+  convBuf<ac_int<8,false>,10,10,7,7>(array,dil_filter,result);
 
-  std::cout<<"\n BUFFER TIME! \n";
-  convBuf<ac_int<8,false>,10,10,7,7>(array,dil_filter,result2);
-  for(int i=0;i<10;i++){
-	  for(int j=0;j<10;j++){
-	  	std::cout<<result2[i][j]<<" ";
-	  }
-	  std::cout<<"\n";
-	}
   return 0;
 }
